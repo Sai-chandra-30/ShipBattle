683ac4e9b8cf2f8c7dbcdbe10b03c814ec23c30e
Sristhuthi Chakravarthy <sristhuthichakravarthy@Sristhuthis-MacBook-Air.local>
2026-04-05 21:23:15 -0400

    Sprint 1

---
+++ Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        boolean gameStart = false;
+
+        while (!gameStart) {
+            System.out.print("Start Game? (y/n): ");
+            char userInput1 = Character.toLowerCase(scnr.next().charAt(0));
+
+            if (userInput1 == 'y') {
+                gameStart = true;
+            }
+            else if (userInput1 == 'n') {
+                System.exit(0);
+            }
+            else {
+                System.out.println("Please enter y or n");
+            }
+        }
+
+        Board b1 = new Board();
+        b1.displayBoardCoords();
+
+        boolean validShip = false;
+        char userInput2 = '0';
+        System.out.println("\nCarrier: (c)\nBattleship: (b)\nDestroyer: (d)\nSubmarine: (s)\nFrigate: (f)");
+
+        while (!validShip) {
+            System.out.print("Choose Ship to place: ");
+            userInput2 = Character.toLowerCase(scnr.next().charAt(0));
+
+            switch (userInput2) {
+                case 'c':
+                case 'b':
+                case 'd':
+                case 's':
+                case 'f':
+                    validShip = true;
+                    break;
+                default:
+                    System.out.println("Please enter a valid letter");
+            }
+        }
+
+        boolean validCoordinate = false;
+        String userInput3 = "";
+
+        while (!validCoordinate) {
+            System.out.print("Select coordinate: ");
+            userInput3 = scnr.next().toUpperCase();
+
+            if (b1.verifyCoord(userInput3)) {
+                validCoordinate = true;
+            }
+            else {
+                System.out.println("Enter valid coordinate.");
+            }
+        }
+
+        b1.addShip(userInput3, userInput2);
+
+        System.out.println("\nUpdated Board:");
+        b1.displayBoardShips();
+
+        scnr.close();
+    }
+}
