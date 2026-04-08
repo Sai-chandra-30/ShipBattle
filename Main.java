12ee435e9e23c6792f03180a440cc9ed75771890
Connor Harrison <115675068+PekkaBoi38@users.noreply.github.com>
2026-04-08 18:13:28 -0400

    Finalized Ship Randomization
    Editing RandomShip.java to work with Main.java and Board.java. Added a getter method for the board grid in Board.java. Made it so that Main.java could choose between manual picking and random picking.

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         Scanner scnr = new Scanner(System.in);
         boolean gameStart = false;
+        boolean random = false;
 
         while (!gameStart) {
             System.out.print("Start Game? (y/n): ");
@@ -22,48 +23,70 @@ public class Main {
             }
         }
 
-        Board b1 = new Board();
-        b1.displayBoardCoords();
+        gameStart = false;
 
-        boolean validShip = false;
-        char userInput2 = '0';
-        System.out.println("\nCarrier: (c)\nBattleship: (b)\nDestroyer: (d)\nSubmarine: (s)\nFrigate: (f)");
+        while (!gameStart) {
+            System.out.print("Pick a place for a ship, or do it randomly? (p/r): ");
+            char userInput1 = Character.toLowerCase(scnr.next().charAt(0));
 
-        while (!validShip) {
-            System.out.print("Choose Ship to place: ");
-            userInput2 = Character.toLowerCase(scnr.next().charAt(0));
-
-            switch (userInput2) {
-                case 'c':
-                case 'b':
-                case 'd':
-                case 's':
-                case 'f':
-                    validShip = true;
-                    break;
-                default:
-                    System.out.println("Please enter a valid letter");
+            if (userInput1 == 'p') {
+                gameStart = true;
             }
-        }
-
-        boolean validCoordinate = false;
-        String userInput3 = "";
-
-        while (!validCoordinate) {
-            System.out.print("Select coordinate: ");
-            userInput3 = scnr.next().toUpperCase();
-
-            if (b1.verifyCoord(userInput3)) {
-                validCoordinate = true;
+            else if (userInput1 == 'r') {
+                gameStart = true;
+                random = true;
             }
             else {
-                System.out.println("Enter valid coordinate.");
+                System.out.println("Please enter p or r");
             }
         }
 
-        b1.addShip(userInput3, userInput2);
+        Board b1 = new Board();
+        if(!random) {
+            b1.displayBoardCoords();
 
-        System.out.println("\nUpdated Board:");
+            boolean validShip = false;
+            char userInput2 = '0';
+            System.out.println("\nCarrier: (c)\nBattleship: (b)\nDestroyer: (d)\nSubmarine: (s)\nFrigate: (f)");
+
+            while (!validShip) {
+                System.out.print("Choose Ship to place: ");
+                userInput2 = Character.toLowerCase(scnr.next().charAt(0));
+
+                switch (userInput2) {
+                    case 'c':
+                    case 'b':
+                    case 'd':
+                    case 's':
+                    case 'f':
+                        validShip = true;
+                        break;
+                    default:
+                        System.out.println("Please enter a valid letter");
+                }
+            }
+
+            boolean validCoordinate = false;
+            String userInput3 = "";
+
+            while (!validCoordinate) {
+                System.out.print("Select coordinate: ");
+                userInput3 = scnr.next().toUpperCase();
+
+                if (b1.verifyCoord(userInput3)) {
+                    validCoordinate = true;
+                } else {
+                    System.out.println("Enter valid coordinate.");
+                }
+            }
+
+            b1.addShip(userInput3, userInput2);
+
+            System.out.println("\nUpdated Board:");
+        }
+        else {
+            b1 = RandomShip.makeShip();
+        }
         b1.displayBoardShips();
 
         scnr.close();
