01cf718606a357aef4e6e9bca6f89c758c836911
TheCheeseler <115675068+PekkaBoi38@users.noreply.github.com>
2026-04-19 15:48:56 -0400

    Created a sample turnflow for the player and bot
    Made a sample turnflow, where the player can enter coordinates to strike areas on the board. Hits and misses are signified after each strike, and sunken ships are announced if any. The bot strikes in a random legal position each turn. When no ships remain on either side, the winner, along with their amount of remaining pieces, are announced and the game ends.

---
+++ GameManager.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+
+public class GameManager {
+    public static int playerHP = 17;
+    public static int botHP = 17;
+    public static Scanner scnr = new Scanner(System.in);
+
+    public static void playGame(Board playerBoard, Board botBoard) {
+        while(playerHP > 0 && botHP > 0) {
+            botBoard = playerStrike(botBoard);
+            playerBoard = botStrike(playerBoard);
+        }
+        if(playerHP == 0) {
+            System.out.println("Bot has won! " + botHP + " ship coordinates remain unharmed");
+        }
+        if(botHP == 0) {
+            System.out.println("Player has won! " + playerHP + " ship coordinates remain unharmed");
+        }
+        scnr.close();
+    }
+
+    public static Board playerStrike(Board botBoard) {
+        int[] coords;
+        boolean valid = false;
+        while(!valid) {
+            coords = getPlayerCoords();
+            if(botBoard.getCell(coords[0],coords[1]) == Cell.HIT || botBoard.getCell(coords[0],coords[1]) == Cell.MISS) {
+                System.out.println("ERROR: Area already targeted. Please select unique coordinates.");
+            }
+            else if(botBoard.getCell(coords[0],coords[1]) != Cell.WATER) {
+                Cell hit = botBoard.getCell(coords[0],coords[1]);
+                botBoard.setCell(coords[0],coords[1], Cell.HIT);
+                announceSink(hit, botBoard);
+                System.out.println("It's a HIT! " + hit);
+                botHP--;
+                valid = true;
+            }
+            else {
+                botBoard.setCell(coords[0],coords[1], Cell.MISS);
+                System.out.println("It's a miss...");
+                valid = true;
+            }
+            botBoard.displayBoardShips();
+        }
+        return botBoard;
+    }
+
+    public static Board botStrike(Board playerBoard) {
+        int[] coords = new int[2];
+        boolean valid = false;
+        while(!valid) {
+            coords[0] = (int) (Math.random() * 10);
+            coords[1] = (int) (Math.random() * 10);
+            if(playerBoard.getCell(coords[0],coords[1]) == Cell.HIT || playerBoard.getCell(coords[0],coords[1]) == Cell.MISS) {}
+            else if(playerBoard.getCell(coords[0],coords[1]) != Cell.WATER) {
+                System.out.println("Striking at coordinates " + coords[0] + coords[1] + "...");
+                Cell hit = playerBoard.getCell(coords[0],coords[1]);
+                playerBoard.setCell(coords[0],coords[1], Cell.HIT);
+                announceSink(hit, playerBoard);
+                System.out.println("It's a HIT! " + hit);
+                playerHP--;
+                valid = true;
+            }
+            else {
+                System.out.println("Striking at coordinates " + (char) (coords[0] + 65) + (coords[1] + 1) + "...");
+                playerBoard.setCell(coords[0],coords[1], Cell.MISS);
+                System.out.println("It's a miss...");
+                valid = true;
+            }
+        }
+        playerBoard.displayBoardShips();
+        return playerBoard;
+    }
+
+    public static int[] getPlayerCoords() {
+        int[] coords = new int[2];
+        boolean valid = false;
+        char letter = ' ';
+        int num = 0;
+        while(!valid) {
+            System.out.println("Enter coordinates to strike at. Format input like this: C3 <A-J><1-10>");
+            String input = scnr.nextLine();
+            letter = input.toUpperCase().charAt(0);
+            num = Integer.parseInt(input.substring(1));
+            System.out.println("Row: " + letter + (int) letter);
+            System.out.println("Column: " + num);
+            if(((int) letter) - 65 < 0 || ((int) letter) - 65 > 9) {
+                System.out.println("ERROR: Invalid Row Coordinate. Make sure the character you enter is between A and J.");
+            }
+            else if(num < 1 || num > 10) {
+                System.out.println("ERROR: Invalid Column Coordinate. Make sure the number you enter is between 1 and 10.");
+            }
+            else {
+                valid = true;
+                System.out.println("Striking at coordinates " + letter + num + "...");
+            }
+        }
+        coords[0] = ((int) letter) - 65;
+        coords[1] = num - 1;
+        return coords;
+    }
+
+    public static void announceSink(Cell c, Board b) {
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                if(b.getCell(i, j) == c) {
+                    return;
+                }
+            }
+        }
+        System.out.println(c + " has been sunk!");
+    }
+
+
+}
