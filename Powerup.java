8f1bf97532114212a5fec1917b4968861426f45c
lnorth2 <lnorth2@gmu.edu>
2026-04-27 06:07:22 -0400

    Update Powerup.java

--- Powerup.java
+++ Powerup.java
@@ -1,75 +1,30 @@
 public class Powerup {
 
-    //Int representing which powerup this is
-    //  0 = Radar
-    //  1 = Shield
-    //  2 = Reeinforcements
-    //  3 = Communication Disruption
-    //  4 = Blackout
-    //  5 = Rebuild
-    //  6 = Reposition
-    int type;
-
-    //Constructor called when a powerup is generated
-    public Powerup(){
-        type = (int)(Math.random() * 7);
-    }
-
-    //Public method called when the powerup is used - switches into the correct one for the type
-    public void usePowerup(){
-        switch(type){
-            case 0:
-                doRadar();
-                break;
-            case 1:
-                doShield();
-                break;
-            case 2:
-                doReeinforcements();
-                break;
-            case 3:
-                doCommunicationDisruption();
-                break;
-            case 4:
-                doBlackout();
-                break;
-            case 5:
-                doRebuild();
-                break;
-            case 6:
-                doReposition();
-                break;
-        }
-    }
-
-    private void doRadar(){
+    public void doRadar(){
         //TO DO
     }
 
-    private void doShield(){
+    public void doShield(){
         //TO DO
     }
 
-    private void doReeinforcements(){
+    public void doReeinforcements(){
         //TO DO
     }
 
-    private void doCommunicationDisruption(){
+    public void doCommunicationDisruption(){
         //TO DO
     }
 
-    private void doBlackout(){
-        
+    public void doBlackout(){
+        //TO DO (Liam)
     }
 
-    private void doRebuild(){
-
+    public void doRebuild(){
+        //TO DO (Liam)
     }
 
-    private void doReposition(){
-
+    public void doReposition(){
+        //TO DO (Liam)
     }
-
-
-
 }
