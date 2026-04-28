547b1cbf943e9daf5a46e89389bc88cb89008205
lnorth2 <lnorth2@gmu.edu>
2026-04-28 11:51:29 -0400

    Put powerup code here

---
+++ Powerup.java
@@ -0,0 +1,30 @@
+public class Powerup {
+    
+    public static void doRadar(){
+        //TO DO
+    }
+
+    public static void doShield(){
+        //TO DO
+    }
+
+    public static void doReinforcements(){
+        //TO DO
+    }
+
+    public static void doCommunicationDisruption(){
+        //TO DO
+    }
+
+    public static void doBlackout(){
+        //TO DO (Liam)
+    }
+
+    public static void doRebuild(){
+        //TO DO (Liam)
+    }
+
+    public static void doReposition(){
+        //TO DO (Liam)
+    }
+}
